0d3b54549fd7b608ff23a71826cc79d214e9abb3
Chrissy <77197824+Chrissy-buzz@users.noreply.github.com>
2021-01-25 16:44:00 -0500

    Update Ariana.py

--- Ariana.py
+++ Ariana.py
@@ -1,1 +1,81 @@
 
+import tweepy
+from twilio.rest import Client
+
+#Authentification from Twilio
+account_sid = "Please access with your account_sid"
+# Your Auth Token from twilio.com/console
+auth_token  = "Please access with your auth_token"
+
+client = Client(account_sid, auth_token)
+
+#Authentification for Tweepy
+auth = tweepy.OAuthHandler("KGGTMeLRi8ruRi3ZOkZv5h59I", "hCdXoXrEmacwDPSNEZxFY5cSoWfhp9dTUGzuOrmfF0NVAEsU0t")
+auth.set_access_token("1352178353655193602-7Mj69IHTeQ6JPHIcMGlii4bd71jchD", "DCKN4xt0j1sCgJRDrmct5WmJ6gxiB1VHrgDf8XosJ0w4s")
+
+# DEBUG Verify Twitter Credentials
+#try:
+#    api.verify_credentials()
+#    print("Authentication OK")
+#except:
+#    print("Error during authentication")
+
+#Filters out mentions and RTs
+def from_creator(status):
+    if hasattr(status, 'retweeted_status'):
+        return False
+    elif status.in_reply_to_status_id != None:
+        return False
+    elif status.in_reply_to_screen_name != None:
+        return False
+    elif status.in_reply_to_user_id != None:
+        return False
+    else:
+        return True
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, status):
+        if from_creator(status):
+            print("🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍")
+            print(f"{status.text}")
+            txt=status.text
+            if txt=="tour" or "Tour":
+                print("Shes going on tour 😄")
+            username = status.user.screen_name
+            print(username)
+            hashtags = status.entities['hashtags']
+            if hashtags:
+                print(hashtags)
+            else:
+                print("☀")
+            acctdesc = status.user.description
+            if acctdesc:
+                print(acctdesc)
+            else:
+                print("⭐")
+            location = status.user.location
+            if location:
+                print(location)
+            else:
+                print("❣")
+            print("🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍🤍")
+            #message = client.messages.create(to="+1yournumber", from_ ="+1twilionumber", body = txt)
+            #print(message)
+            following = status.user.friends_count
+            followers = status.user.followers_count
+            totaltweets = status.user.statuses_count
+            usercreatedts = status.user.created_at
+            tweetcreatedts = status.created_at
+            retweetcount = status.retweet_count
+
+
+    def on_error(self, status):
+        print("Error detected")
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(follow=["34507480"]) #you can change the user ID to track other twitter users
